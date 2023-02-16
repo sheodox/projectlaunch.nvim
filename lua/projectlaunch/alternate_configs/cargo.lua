fd242e8d4f7c445b2bea4593e9c0907cdad5ec32
erven2016 <leiguihua2016@gmail.com>
2023-02-16 13:10:39 +0800

    feat: add alternative config of rust cargo

---
+++ lua/projectlaunch/alternate_configs/cargo.lua
@@ -0,0 +1,47 @@
+local M = {}
+-- local path = require("projectlaunch.path")
+local config_utils = require("projectlaunch.config_utils")
+
+M.name = "Cargo"
+M.runner = "cargo"
+
+M.is_runner_executable = function()
+	if vim.fn.executable(M.runner) == 1 then
+		return true
+	end
+	return false
+end
+
+M.is_detected = function(project_files)
+	-- matching `src/` and `Cargo.toml`
+	if vim.tbl_contains(project_files, "src") and vim.tbl_contains(project_files, "Cargo.toml") then
+		return true
+	end
+	return false
+end
+
+M.get_config = function(project_files)
+	if M.is_detected(project_files) then
+		if not M.is_runner_executable() then
+			vim.notify("ProjectLaunch: " .. M.runner .. " is not executable.")
+		end
+
+		-- available cargo commands in project scope
+		local available_commands =
+			{ "run", "build", "build --release", "check", "clean", "init", "update", "test", "bench" }
+
+		local config = {
+			commands = {},
+		}
+
+		for i = 1, #available_commands, 1 do
+			table.insert(config.commands, { cmd = M.runner .. " " .. available_commands[i] })
+		end
+
+		return config_utils.Config:new(config)
+	end
+
+	return nil
+end
+
+return M
