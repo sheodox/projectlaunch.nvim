a6f13f2fdd55d3cc154ca7226078f0cb11f7b938
ToaLewa <kooltone16@gmail.com>
2023-06-30 14:45:55 -0500

    Prompt to set variables in config at runtime

--- lua/projectlaunch/jobs.lua
+++ lua/projectlaunch/jobs.lua
@@ -5,7 +5,12 @@ local api = vim.api
 local term_name_prefix = "ProjectLaunch terminal - "
 
 local Job = {}
+runtimeVars = {}
+
 function Job:new(command, opts)
+    runtimeVars = {}
+    local finalCommand = generateCommand(command)
+	
 	local temp_win, buf = win.create_temp_window()
 	vim.opt_local.spell = false
 
@@ -20,8 +25,8 @@ function Job:new(command, opts)
 		-- duplicating the name lets commands be edited and
 		-- jobs won't get the updated command until it's rerun,
 		-- so it won't look like the newly edited command had been run automatically
-		name = command.name,
-		cmd = command.cmd,
+		name = generateJobName(command),
+		cmd = finalCommand,
 		job_id = nil,
 		buf = buf,
 		running = true,
@@ -30,7 +35,7 @@ function Job:new(command, opts)
 		_args = { command, opts },
 	}
 
-	j.job_id = vim.fn.termopen(vim.split(command.cmd, " "), {
+	j.job_id = vim.fn.termopen(vim.split(finalCommand, " "), {
 		cwd = cwd,
 		on_exit = function(_, exit_code)
 			j.running = false
@@ -55,4 +60,53 @@ function Job:kill()
 	vim.fn.jobstop(self.job_id)
 end
 
+function generateCommand(originalCommand) 
+	local finalCommand = originalCommand.cmd 
+
+    for i = 1,5,1
+    do
+        local substitutionString = "$" .. i
+
+        if string.find(originalCommand.cmd, substitutionString) then
+            setRuntimeVarViaPrompt(substitutionString, finalCommand)
+            finalCommand = string.gsub(finalCommand, substitutionString, runtimeVars[i])
+        end
+    end
+
+    return finalCommand
+end
+
+function generateJobName(command) 
+    if(table.getn(runtimeVars) > 0) then
+       local args = "" 
+       
+       for key, value in pairs(runtimeVars) do
+          args = args .. value .. "," 
+       end 
+
+       args = args:sub(1, -2) --Remove trailing comma
+
+       return command.name .. " (" .. args .. ")"
+    end 
+
+    return command.name
+end
+
+
+function setRuntimeVarViaPrompt(var, command)
+    vim.ui.input({ prompt = "ProjectLaunch: Enter argument " .. var .. " for\n" .. command .. "\n:"}, function(input)
+        table.insert(runtimeVars, input) 
+    end)
+end
+
+
+function replaceVariableInCommand(var, finalCommand)
+    vim.ui.input({ prompt = "ProjectLaunch: Enter argument " .. var .. ": "}, function(cmd)
+        finalCommand = string.gsub(finalCommand, var, cmd)
+    end)
+
+    return finalCommand
+end
+
 return Job
+
